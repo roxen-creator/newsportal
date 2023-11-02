55a141bb110093242c8bf3b4601cee87517feb4a
Rosan <frankroyal41@gmail.com>
2023-11-02 20:21:47 +0545

    navbar updated

--- includes/header.php
+++ includes/header.php
@@ -4,16 +4,36 @@
 <head>
   <!-- Add Bootstrap CSS link -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
+  <style>
+    .navbar .navbar-category .nav-link.active {
+      font-weight: bold;
+      /* Customize the style for the active tab */
+      color: #007bff;
+      /* Customize the color for the active tab */
+    }
+  </style>
 </head>
 
 <body>
+  <?php
+  // Define the current page (you may adjust this based on your URL structure)
+  $currentPage = basename($_SERVER['PHP_SELF']);
+
+  // Initialize $currentCategory as null
+  $currentCategory = null;
+
+  if (isset($_GET['catid'])) {
+    // Define the current category based on the URL parameter 'catid'
+    $currentCategory = $_GET['catid'];
+  }
+  ?>
+
   <nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
     <div class="container">
       <div class="navbar-left">
         <span id="datetime" class="nav-link text-center"></span>
       </div>
-      <div class="navbar-center">
+      <div class "navbar-center">
         <a class="navbar-brand" href="index.php">
           <img src="images/logo.png" height="80" width="152">
         </a>
@@ -38,37 +58,46 @@
         <div class="navbar-category" style="padding-top: 55px;">
           <div class="navbar-category-list d-flex justify-content-center flex-wrap collapse navbar-collapse"
             id="navbarNavCategory">
-            <a href="index.php" class="nav-link" style="margin-right: 15px; margin-bottom: 10px;"> Home </a>
+            <style>
+              .nav-link {
+                border: 1px solid #ccc;
+                /* Add a subtle border to the nav-links */
+                border-radius: 5px;
+                /* Add rounded corners for a cleaner look */
+                padding: 5px 10px;
+                /* Adjust padding for spacing */
+                margin-right: 15px;
+                margin-bottom: 10px;
+              }
+            </style>
+            <a href="index.php" class="nav-link <?php if ($currentPage === 'index.php')
+              echo 'active'; ?>"> Home </a>
 
             <?php
             $query = mysqli_query($con, "select id, CategoryName from tblcategory");
             while ($row = mysqli_fetch_array($query)) {
-              ?>
-              <a href="category.php?catid=<?php echo htmlentities($row['id']); ?>" class="nav-link"
-                style="margin-right: 15px; margin-bottom: 10px;">
-                <?php echo htmlentities($row['CategoryName']); ?>
-              </a>
-            <?php } ?>
+              $categoryId = $row['id'];
+              $categoryName = htmlentities($row['CategoryName']);
+              $activeClass = ($currentCategory === $categoryId) ? 'active' : '';
+              echo '<a href="category.php?catid=' . $categoryId . '" class="nav-link ' . $activeClass . '">' . $categoryName . '</a>';
+            }
+            ?>
           </div>
         </div>
       </div>
     </div>
   </div>
-
-  <!-- Add Bootstrap JavaScript and jQuery libraries -->
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
+
 
 <script>
   function updateDateTime() {
     const datetimeElement = document.getElementById("datetime");
     const currentDate = new Date();
 
-    // Get the options to format the date
+
     const options = {
       weekday: 'long', // 'short', 'narrow' can also be used
       year: 'numeric',
@@ -83,7 +112,6 @@
   }
 
   updateDateTime();
-
-  // Update the date and time every second
   setInterval(updateDateTime, 1000);
+
 </script>
