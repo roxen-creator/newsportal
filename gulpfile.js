38995b4250d1fbfdc00436add9e14d4168382c10
Rosan <frankroyal41@gmail.com>
2023-11-02 20:00:48 +0545

    ui updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,41 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var pkg = require("./package.json");
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
+gulp.task("vendor", function () {
   // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+  gulp
+    .src([
+      "./node_modules/bootstrap/dist/**/*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-grid*",
+      "!./node_modules/bootstrap/dist/css/bootstrap-reboot*",
     ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+    .pipe(gulp.dest("./vendor/bootstrap"));
 
   // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
+  gulp
+    .src([
+      "./node_modules/jquery/dist/*",
+      "!./node_modules/jquery/dist/core.js",
     ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
-})
+    .pipe(gulp.dest("./vendor/jquery"));
+});
 
 // Default task
-gulp.task('default', ['vendor']);
+gulp.task("default", ["vendor"]);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task("browserSync", function () {
   browserSync.init({
     server: {
-      baseDir: "./"
-    }
+      baseDir: "./",
+    },
   });
 });
 
 // Dev task
-gulp.task('dev', ['browserSync'], function() {
-  gulp.watch('./css/*.css', browserSync.reload);
-  gulp.watch('./*.html', browserSync.reload);
+gulp.task("dev", ["browserSync"], function () {
+  gulp.watch("./css/*.css", browserSync.reload);
+  gulp.watch("./*.html", browserSync.reload);
 });
